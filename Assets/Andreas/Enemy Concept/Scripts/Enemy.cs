fa7b60a8fe02eb5a110292eec21c6a41edd95143
Xgor <real.xgor@hotmail.com>
2016-07-06 18:52:30 +0200

    Inherence from BaseEnemy

--- Assets/Andreas/Enemy Concept/Scripts/Enemy.cs
+++ Assets/Andreas/Enemy Concept/Scripts/Enemy.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : FreBaseEnemy
 {
     private Vector3 targetLocation;
 
@@ -30,10 +30,6 @@ public class Enemy : MonoBehaviour
             Move();
         }
 
-        if (life <= 0)
-        {
-            DestroyEnemy();
-        }
     }
 
     public virtual void Shoot()
@@ -66,12 +62,6 @@ public class Enemy : MonoBehaviour
         return RotateAroundPoint(playerDummy.transform.position, transform.position, Quaternion.Euler(0, 0, Random.Range(-80, 80)));
     }
 
-    public virtual void DestroyEnemy()
-    {
-        // handle destruction
-
-        Destroy(gameObject);
-    }
 
     public Vector2 GetNewTargetLocation()
     {
