09e3aa6fbc436257fd956d785f8c7a0f5b172c4e
Oscar Losada <oscarlosu@gmail.com>
2016-07-06 23:03:43 +0200

    Basic progress bar for loading.

--- Assets/Oscar/SceneLoading/SceneLoader.cs
+++ Assets/Oscar/SceneLoading/SceneLoader.cs
@@ -13,6 +13,10 @@ public class SceneLoader : MonoBehaviour {
     public bool UseButton = false;
     public string ButtonName = "Submit";
 
+    public UnityEngine.UI.Image ProgressBar;
+
+    public float ProgressBarSpeed;
+
     public delegate void LoadEventHandler();
     public event LoadEventHandler LoadStarted;
     public event LoadEventHandler LoadEnded;
@@ -44,12 +48,23 @@ public class SceneLoader : MonoBehaviour {
         asyncOp.allowSceneActivation = false;
         // Progress animation
         int count = 0;
-        while (asyncOp.progress < 0.9f) {
+        RectTransform rect = ProgressBar.GetComponent<RectTransform>();
+        float scale = 0;
+        while (scale < Screen.width) {
             ++count;
+            if(asyncOp.progress < 0.9f) {
+                scale = Mathf.Min(Screen.width * asyncOp.progress, scale + ProgressBarSpeed * Time.deltaTime);
+            } else {
+                scale = scale + ProgressBarSpeed * Time.deltaTime;
+            }
+            
+            rect.sizeDelta = new Vector2(scale, 0);
+            
             yield return null;
         }
         // Load end animation
-        Debug.Log("Loading took " + count + " frames");
+        //Debug.Log("Progress " + asyncOp.progress + " * " + Screen.width + " = " + Screen.width * asyncOp.progress);
+        //Debug.Log("Loading took " + count + " frames");
         // Allow scene activation
         asyncOp.allowSceneActivation = true;
         // Trigger load end event
@@ -67,9 +82,11 @@ public class SceneLoaderEditor : Editor {
         //myScript.CurrentSceneIndex = EditorGUILayout.IntField("Current Scene Index", myScript.CurrentSceneIndex);
         myScript.NextSceneIndex = EditorGUILayout.IntField("Next Scene Index", myScript.NextSceneIndex);
         myScript.UseButton = GUILayout.Toggle(myScript.UseButton, "Use Button");
-        if(myScript.UseButton) {
+        if (myScript.UseButton) {
             myScript.ButtonName = EditorGUILayout.TextField("Button Name", myScript.ButtonName);
-        }        
+        }
+        myScript.ProgressBar = (UnityEngine.UI.Image)EditorGUILayout.ObjectField("Progress Bar", myScript.ProgressBar, typeof(UnityEngine.UI.Image), true);
+        myScript.ProgressBarSpeed = EditorGUILayout.FloatField("Progress Bar Speed", myScript.ProgressBarSpeed);
     }
 }
 #endif
