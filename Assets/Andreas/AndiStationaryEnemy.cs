e7f9af627aba935c24488622576a774a63d40579
Andreas Pritschet <andreas@pritschet.eu>
2016-07-07 19:59:45 +0200

    new enemy type

---
+++ Assets/Andreas/AndiStationaryEnemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class AndiStationaryEnemy : FreBaseEnemy
+{
+    public GameObject[] childParts;
+    public GameObject centerObject;
+    public float rotationSpeed;
+    private Vector3 point;
+    private Vector3 pivot;
+
+    void Update()
+    {
+        float sine = Mathf.Abs(Mathf.Sin(Time.time * 10)) - 0.5f;
+        float cosine = Mathf.Abs(Mathf.Cos(Time.time * 10)) - 0.5f;
+        centerObject.GetComponent<SpriteRenderer>().color = new Color(1, sine, sine);
+
+        centerObject.transform.Rotate(new Vector3(0, 0, -3));
+
+        foreach (GameObject child in childParts)
+        {
+            child.transform.RotateAround(transform.position, Vector3.forward, rotationSpeed);
+            child.GetComponent<SpriteRenderer>().color = new Color(1, cosine, cosine);
+        }
+    }
+}
