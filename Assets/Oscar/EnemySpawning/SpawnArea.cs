d8b718774ca08db33f296a9d3f970159eb396bb5
Oscar Losada <oscarlosu@gmail.com>
2016-07-07 18:25:43 +0200

    Better gizmos for SpawnAreas.

--- Assets/Oscar/EnemySpawning/SpawnArea.cs
+++ Assets/Oscar/EnemySpawning/SpawnArea.cs
@@ -13,7 +13,7 @@ public class SpawnArea: MonoBehaviour {
     public SpawnPattern Pattern;
 
     public int EnemyPrefabIndex;
-    public ObjectColor Color;
+    public ObjectColor EnemyColor;
 
     [Tooltip("Not used by Unit pattern.")]
     public int EnemyAmount;
@@ -50,16 +50,16 @@ public class SpawnArea: MonoBehaviour {
         if(!used && col.tag == "Player") {
             switch (Pattern) {
                 case SpawnPattern.Point:
-                    StartCoroutine(spawner.PointSpawner(transform.position, EnemyAmount, Period, EnemyPrefabIndex, Color));
+                    StartCoroutine(spawner.PointSpawner(transform.position, EnemyAmount, Period, EnemyPrefabIndex, EnemyColor));
                     break;
                 case SpawnPattern.Line:
-                    StartCoroutine(spawner.LineSpawner(transform.position, transform.position + LineDirection, EnemyAmount, Period, EnemyPrefabIndex, Color));
+                    StartCoroutine(spawner.LineSpawner(transform.position, transform.position + LineDirection, EnemyAmount, Period, EnemyPrefabIndex, EnemyColor));
                     break;
                 case SpawnPattern.Circle:
-                    StartCoroutine(spawner.CircleSpawner(transform.position, Radius, EnemyAmount, Period, EnemyPrefabIndex, Color));
+                    StartCoroutine(spawner.CircleSpawner(transform.position, Radius, EnemyAmount, Period, EnemyPrefabIndex, EnemyColor));
                     break;
                 case SpawnPattern.Unit:
-                    StartCoroutine(spawner.UnitSpawner(transform.position, ForwardDir, Rows, UnitSpacing, Period, EnemyPrefabIndex, Color));
+                    StartCoroutine(spawner.UnitSpawner(transform.position, ForwardDir, Rows, UnitSpacing, Period, EnemyPrefabIndex, EnemyColor));
                     break;
             }
             used = true;
@@ -67,7 +67,50 @@ public class SpawnArea: MonoBehaviour {
     }
 
     void OnDrawGizmos() {
-        Gizmos.DrawIcon(transform.position, "SpawnAreaIcon.png");
+        //Gizmos.DrawIcon(transform.position, "SpawnAreaIcon.png");
+        switch(EnemyColor) {
+            case ObjectColor.Blue:
+                Gizmos.color = Color.blue;
+                break;
+            case ObjectColor.Green:
+                Gizmos.color = Color.green;
+                break;
+            case ObjectColor.Red:
+                Gizmos.color = Color.red;
+                break;
+            case ObjectColor.Yellow:
+                Gizmos.color = Color.yellow;
+                break;
+        }
+        switch (Pattern) {
+            case SpawnPattern.Point:
+                //Gizmos.DrawIcon(transform.position, "EnemyIcon.png");
+                //Gizmos.DrawWireSphere(transform.position, 0.2f);
+                float angle = 0;
+                for (int i = 0; i < 8; ++i) {
+                    Gizmos.DrawLine(transform.position, transform.position + new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0));
+                    angle += 45;
+                }                
+                break;
+            case SpawnPattern.Line:
+                Gizmos.DrawLine(transform.position, transform.position + LineDirection);
+                break;
+            case SpawnPattern.Circle:
+                Gizmos.DrawWireSphere(transform.position, Radius);
+                break;
+            case SpawnPattern.Unit:
+                Vector3 nDir = ForwardDir.normalized;
+                Vector3 orthoDir = new Vector3(nDir.y, -nDir.x, 0);
+                int rowSize = -1;
+                float sideOffset, frontOffset;
+                for (int r = 0; r < Rows; ++r) {
+                    rowSize += 2;
+                    frontOffset = r * UnitSpacing;
+                    sideOffset = Mathf.Max(Mathf.Floor(rowSize / 2.0f) * UnitSpacing, 0.1f);
+                    Gizmos.DrawLine(transform.position - orthoDir * sideOffset - nDir * frontOffset, transform.position + orthoDir * sideOffset - nDir * frontOffset);
+                }
+                break;
+        }
     }
 
 }
