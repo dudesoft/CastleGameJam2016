edf796ed5576b473b74a7a72b3c397979decdac6
Oscar Losada <oscarlosu@gmail.com>
2016-07-08 01:27:30 +0200

    Beating Start Screen.

---
+++ Assets/Oscar/SceneLoading/BlinkToBeat.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkToBeat : MonoBehaviour {
+    private UnityEngine.UI.Text text;
+    private bool on = false;
+
+    public Color OnColor;
+    public Color OffColor;
+
+    public float LerpSpeed;
+
+    void Start() {
+        text = GetComponent<UnityEngine.UI.Text>();
+    }
+	// Update is called once per frame
+	void Update () {
+        if (BeatManager.instance.beating) {
+            on = !on;            
+        }
+        if (on) {
+            //text.color = Color.Lerp(text.color, OnColor, LerpSpeed * Time.deltaTime);
+            text.color = OnColor;
+        } else {
+            text.color = Color.Lerp(text.color, OffColor, LerpSpeed * Time.deltaTime);
+        }
+	}
+}
