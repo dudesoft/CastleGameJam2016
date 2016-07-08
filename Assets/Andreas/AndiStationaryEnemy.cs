048ca2efe7561c67ce574162f6743c43bed713c4
Andreas Pritschet <andreas@pritschet.eu>
2016-07-08 23:36:14 +0200

    fixed enemies

--- Assets/Andreas/AndiStationaryEnemy.cs
+++ Assets/Andreas/AndiStationaryEnemy.cs
@@ -12,14 +12,46 @@ public class AndiStationaryEnemy : FreBaseEnemy
     {
         float sine = Mathf.Abs(Mathf.Sin(BeatManager.instance.TimeSinceLastBeat * Mathf.PI)) - 0.5f;
         float cosine = Mathf.Abs(Mathf.Cos(BeatManager.instance.TimeSinceLastBeat * Mathf.PI)) - 0.5f;
-        centerObject.GetComponent<SpriteRenderer>().color = new Color(1, sine, sine);
-
+        if (objectColor == ObjectColor.Red)
+        {
+            centerObject.GetComponent<SpriteRenderer>().color = new Color(1, sine, sine);
+        }
+        else if (objectColor == ObjectColor.Blue)
+        {
+            centerObject.GetComponent<SpriteRenderer>().color = new Color(sine, sine, 1);
+        }
+        else if (objectColor == ObjectColor.Green)
+        {
+            centerObject.GetComponent<SpriteRenderer>().color = new Color(sine, 1, sine);
+        }
+        else if (objectColor == ObjectColor.Yellow)
+        {
+            centerObject.GetComponent<SpriteRenderer>().color = new Color(0.7f, 0.7f, sine);
+        }
         centerObject.transform.Rotate(new Vector3(0, 0, -3));
 
         foreach (GameObject child in childParts)
         {
-            child.transform.RotateAround(transform.position, Vector3.forward, rotationSpeed);
-            child.GetComponent<SpriteRenderer>().color = new Color(1, cosine, cosine);
+            if (objectColor == ObjectColor.Red)
+            {
+                child.transform.RotateAround(transform.position, Vector3.forward, rotationSpeed);
+                child.GetComponent<SpriteRenderer>().color = new Color(1, cosine, cosine);
+            }
+            else if (objectColor == ObjectColor.Blue)
+            {
+                child.transform.RotateAround(transform.position, Vector3.forward, rotationSpeed);
+                child.GetComponent<SpriteRenderer>().color = new Color(cosine, cosine, 1);
+            }
+            else if (objectColor == ObjectColor.Green)
+            {
+                child.transform.RotateAround(transform.position, Vector3.forward, rotationSpeed);
+                child.GetComponent<SpriteRenderer>().color = new Color(cosine, 1, cosine);
+            }
+            else if (objectColor == ObjectColor.Yellow)
+            {
+                child.transform.RotateAround(transform.position, Vector3.forward, rotationSpeed);
+                child.GetComponent<SpriteRenderer>().color = new Color(0.7f, 0.7f, cosine);
+            }
         }
     }
 }
