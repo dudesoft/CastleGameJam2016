f95cd30c6f5432bb167c5df2e1eed0fd5c2e11a6
Andreas Pritschet <andreas@pritschet.eu>
2016-07-08 19:13:33 +0200

    fixed bouncing enemies

--- Assets/Andreas/Enemy Concept/AndiEnemyType1.cs
+++ Assets/Andreas/Enemy Concept/AndiEnemyType1.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class AndiEnemyType1 : FreBaseEnemy
 {
     public float speed = 2;
+    public float speedMultiplier;
     private Rigidbody2D rigidbody;
 
     protected override void Init()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.AddForce(new Vector2(Random.Range(0.1f, 1), Random.Range(0.1f, 1)) * 10, ForceMode2D.Impulse);
     }
 
     void Update()
@@ -19,9 +19,9 @@ public class AndiEnemyType1 : FreBaseEnemy
 
     public virtual void Move()
     {
-        if (Vector3.Magnitude(rigidbody.velocity) < 5)
+        if (BeatManager.instance.beating)
         {
-            rigidbody.AddForce(new Vector2(Random.Range(0.1f, 1), Random.Range(0.1f, 1)) * 10, ForceMode2D.Impulse);
+            rigidbody.AddForce(new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * speedMultiplier, ForceMode2D.Impulse);
         }
     }
 }
