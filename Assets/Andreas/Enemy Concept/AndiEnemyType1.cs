303c5b168284652e6463575508e385fa3be25c4f
Andreas Pritschet <andreas@pritschet.eu>
2016-07-08 19:35:08 +0200

    more enemy stuff

--- Assets/Andreas/Enemy Concept/AndiEnemyType1.cs
+++ Assets/Andreas/Enemy Concept/AndiEnemyType1.cs
@@ -21,6 +21,16 @@ public class AndiEnemyType1 : FreBaseEnemy
     {
         if (BeatManager.instance.beating)
         {
+            float xSpeed = Random.Range(0.5f, 1f);
+            float ySpeed = Random.Range(0.5f, 1f);
+            if (Random.Range(0, 1) == 1)
+            {
+                xSpeed *= -1;
+            }
+            if (Random.Range(0, 1) == 1)
+            {
+                ySpeed *= -1;
+            }
             rigidbody.AddForce(new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * speedMultiplier, ForceMode2D.Impulse);
         }
     }
